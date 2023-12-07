28d201146c3b51c7d3491846188ec7b7a319c454
goswamikeerti <keerti.goswami@imentus.com>
2023-12-07 19:05:32 +0530

    add notification

--- src/screen/notification.js
+++ src/screen/notification.js
@@ -262,150 +262,41 @@ import axios from "axios";
 import { EXPO_PUBLIC_API_URL } from "../constants/constant";
 import { useSelector, useDispatch } from "react-redux";
 import  { useEffect, useState } from "react";
-
-
-
-// const commentsData = [
-//   {
-//     id: 1,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-
-//   {
-//     id: 2,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-
-//   {
-//     id: 3,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 4,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-
-//   {
-//     id: 5,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 6,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 7,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 8,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 9,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 10,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 11,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-//   {
-//     id: 12,
-//     name: "Keerti",
-//     comment:
-//       "Welcome to this beautiful 3-bedroom, 2-bathroom home located in the heart of [Name of the Neighborhood], known for its [mention a unique feature of the neighborhood, such as parks, schools, or nearby amenities]. This property offers a warm and inviting atmosphere with a host of attractive features.",
-//   },
-
-// ];
-
-
-
 const Notifications = () => {
   const [commentsData,setCommentsData]=useState([]);
-  // const [notificationData, setNotificationData] = useState([])
-  // const user = useSelector((state) => state?.user?.user);
-  // let userId = user?.user?._id;
   const navigation = useNavigation();
-
-
-  // useEffect(() => {
-  //   console.log("use effect")
-  //   handleNotification()
-
-  //   return () => {
-
-  //   }
-  // }, [])
-
-  // const handleNotification = async () => {
-  //   let url = `${EXPO_PUBLIC_API_URL}notification/${userId}`;
-
-  //   console.log("url:", url)
-  //   let data = await axios.get(url)
-  //   console.log("data:", data?.data)
-  //   setNotificationData(data?.data)
-  // }
-
-
-
-
-
 
   const renderItem = ({ item }) => (
     <View style={styles.commentLikeSection}>
       <Image
         style={styles.profileImg}
         // source={require("../../assets/comment1.png")}
-        source={{ uri: item?.user?.url }}
+        source={{ uri: item?.post?.url }}
       />
       <View style={styles.commentText}>
         <Text style={styles.name}>{item?.user?.fullName}</Text>
         <Text style={styles.comment}>{item?.comment}</Text>
       </View>
-      {/* <Pressable style={styles.likeSection}>
+      <Pressable style={styles.likeSection}>
         <Icon
           color={"white"}
           onPress={() => handleLike(item._id)}
           name={"thumbs-o-up"}
           size={20}
         />
-      </Pressable> */}
+      </Pressable>
     </View>
   );
 
-  const user = useSelector((state) => state?.user?.user?.user);
-  const userId = user?._id;
-const url=`${EXPO_PUBLIC_API_URL}get-notifications/${userId}`
-  const getNotifications=async ()=>{
+ const user = useSelector((state) => state?.user?.user?.user);
+const userId = user?._id;
+const url=`${EXPO_PUBLIC_API_URL}notification/${userId}`
+  
+
+const getNotifications=async ()=>{
      try {
       const response=await axios.get(url);
-     console.log(response.data,'lsDNLVnlDnlhsLDIilghsildgil');
+     console.log('lsDNLVnlDnlhsLDIilghsildgil',response?.data);
      setCommentsData(response?.data?.data)
      } catch (error) {
       console.log('error',error);
@@ -441,6 +332,7 @@ const url=`${EXPO_PUBLIC_API_URL}get-notifications/${userId}`
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
